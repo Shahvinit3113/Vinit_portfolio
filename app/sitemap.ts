99a73de7950bbcf8de4ea9a3c58f24fa341d415d
Shahvinit3113 <shahvinit315@gmail.com>
2026-05-26 11:16:53 +0530

    changes

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,9 +2,11 @@
 import { MetadataRoute } from 'next';
 import { getDevtoPosts } from '@/lib/devto';
 
+export const dynamic = 'force-dynamic';
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     // Base URL
-    const baseUrl = 'https://vinit.me'; // Replace with actual domain if different
+    const baseUrl = 'vinit-portfolio-b59bz81g3-shah-vinits-projects.vercel.app'; // Replace with actual domain if different
 
     // Static routes
     const routes = [
